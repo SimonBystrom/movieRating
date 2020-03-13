f8fa465cfc3b622cd9bda67d787efc08fde54f2c
SimonBystrom <simonbystrom@Simons-MacBook-Air.local>
2020-03-13 14:12:24 +0900

    Updates

---
+++ precache-manifest.0e5178f177eff4b3432c9f15a3fd3575.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d390773db6431fcc15839e2fc01a120",
+    "url": "/movieRatings/index.html"
+  },
+  {
+    "revision": "55a3380e6ff66b22e91e",
+    "url": "/movieRatings/static/css/2.6a9e0700.chunk.css"
+  },
+  {
+    "revision": "59d2d6af6bf404c1faa9",
+    "url": "/movieRatings/static/css/main.978bee60.chunk.css"
+  },
+  {
+    "revision": "55a3380e6ff66b22e91e",
+    "url": "/movieRatings/static/js/2.703fdb0e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movieRatings/static/js/2.703fdb0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59d2d6af6bf404c1faa9",
+    "url": "/movieRatings/static/js/main.9a78fd46.chunk.js"
+  },
+  {
+    "revision": "2cfc9518234696fcd706",
+    "url": "/movieRatings/static/js/runtime-main.ac5eaf3c.js"
+  },
+  {
+    "revision": "406d0c249bd171c39c61dc073b668f07",
+    "url": "/movieRatings/static/media/fight-club-web.406d0c24.jpg"
+  },
+  {
+    "revision": "3f4e7b0c472130750e728d960d1e5dc8",
+    "url": "/movieRatings/static/media/interstellar-web.3f4e7b0c.jpg"
+  },
+  {
+    "revision": "b2b186b2486fe55de5d85c9e45a17f37",
+    "url": "/movieRatings/static/media/japan-hex.b2b186b2.png"
+  },
+  {
+    "revision": "34ab8e4b8fd2447a24f20b64f06f0dc1",
+    "url": "/movieRatings/static/media/remixicon.34ab8e4b.woff2"
+  },
+  {
+    "revision": "35f2248c627ac576eded5c607221b7dc",
+    "url": "/movieRatings/static/media/remixicon.35f2248c.woff"
+  },
+  {
+    "revision": "98e2b68082a8c48c239b0f859700e275",
+    "url": "/movieRatings/static/media/remixicon.98e2b680.svg"
+  },
+  {
+    "revision": "9d0a042e6a7a855c0e5d3b233fce41f7",
+    "url": "/movieRatings/static/media/remixicon.9d0a042e.ttf"
+  },
+  {
+    "revision": "e557a8c5256ae63162abc8ec043f348f",
+    "url": "/movieRatings/static/media/remixicon.e557a8c5.eot"
+  },
+  {
+    "revision": "72cc71a6789a4eb57175cbfb766a5491",
+    "url": "/movieRatings/static/media/united-kingdom-hex.72cc71a6.png"
+  },
+  {
+    "revision": "cec2ca9a64bfd10bcef37ae7f395b476",
+    "url": "/movieRatings/static/media/united-kingdom.cec2ca9a.png"
+  }
+]);
