35c3aa4320864d0583a0eefe961290e49a38eb8b
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-21 10:05:11 +0900

    Updated gitignore

--- movieratings/src/getSearchResults.js
+++ movieratings/src/getSearchResults.js
@@ -1,9 +1,10 @@
 // Fetches search results (used by MovieSearched in it's async method)
+import { API_KEY } from './api'
 
 export async function getSearchResults(searchTarget, searchLanguage){
     
 
-    let results = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=ab85baadb27ea7d2eade887860bfa03a&language=${searchLanguage}&query=
+    let results = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=${searchLanguage}&query=
     ${searchTarget}&page=1&include_adult=false`).then(res => res.json())
 
     
